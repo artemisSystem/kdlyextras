82f42c8cc205da1011e21970b1bb2c87545f05ab
Artemis System <theArtemisSystem@gmail.com>
2022-11-30 08:48:56 +0100

    convert DensityFunctionVisitor to a record

--- src/main/java/artemis/kdlyextras/worldgen/KdlyExtrasDensityFunctionVisitor.java
+++ src/main/java/artemis/kdlyextras/worldgen/KdlyExtrasDensityFunctionVisitor.java
@@ -1,29 +1,14 @@
 package artemis.kdlyextras.worldgen;
 
 import net.minecraft.core.Holder;
-import net.minecraft.resources.ResourceKey;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.levelgen.DensityFunction;
-import net.minecraft.world.level.levelgen.DensityFunctions;
-import net.minecraft.world.level.levelgen.Noises;
 import net.minecraft.world.level.levelgen.RandomState;
-import net.minecraft.world.level.levelgen.synth.BlendedNoise;
 import net.minecraft.world.level.levelgen.synth.NormalNoise;
+import org.jetbrains.annotations.NotNull;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Optional;
 
 // TODO: Document that legacy stuff is not supported
-public class KdlyExtrasDensityFunctionVisitor implements DensityFunction.Visitor {
-
-	private final RandomState randomState;
-
-	KdlyExtrasDensityFunctionVisitor(RandomState randomState) {
-		this.randomState = randomState;
-	}
+public record KdlyExtrasDensityFunctionVisitor(RandomState randomState) implements DensityFunction.Visitor {
 
 	@Override
 	public DensityFunction.NoiseHolder visitNoise(DensityFunction.NoiseHolder noiseHolder) {
@@ -34,7 +19,7 @@ public class KdlyExtrasDensityFunctionVisitor implements DensityFunction.Visitor
 	}
 
 	@Override
-	public DensityFunction apply(DensityFunction densityFunction) {
+	public DensityFunction apply(@NotNull DensityFunction densityFunction) {
 		return densityFunction;
 	}
 }
