0f5b25de6f7bf74f39f1deac57440c9e75dd43c2
Artemis System <theArtemisSystem@gmail.com>
2022-11-30 08:49:24 +0100

    Extract density function logic into its own…
    …abstract class

--- src/main/java/artemis/kdlyextras/worldgen/DensityFunctionCountPlacement.java
+++ src/main/java/artemis/kdlyextras/worldgen/DensityFunctionCountPlacement.java
@@ -1,58 +1,29 @@
 package artemis.kdlyextras.worldgen;
 
-import artemis.kdlyextras.KdlyExtras;
 import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Registry;
 import net.minecraft.util.Mth;
 import net.minecraft.util.RandomSource;
-import net.minecraft.world.level.chunk.ChunkGenerator;
 import net.minecraft.world.level.levelgen.DensityFunction;
-import net.minecraft.world.level.levelgen.NoiseBasedChunkGenerator;
-import net.minecraft.world.level.levelgen.NoiseGeneratorSettings;
-import net.minecraft.world.level.levelgen.RandomState;
 import net.minecraft.world.level.levelgen.placement.PlacementContext;
-import net.minecraft.world.level.levelgen.placement.PlacementModifier;
 import net.minecraft.world.level.levelgen.placement.PlacementModifierType;
 
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
-public class DensityFunctionCountPlacement extends PlacementModifier {
-	private final DensityFunction densityFunction;
-
+public class DensityFunctionCountPlacement extends DensityFunctionPlacementModifier {
 	private DensityFunctionCountPlacement(DensityFunction densityFunction) {
-		this.densityFunction = densityFunction;
+		super(densityFunction);
 	}
 
-	public static final Codec<DensityFunctionCountPlacement> CODEC = DensityFunction.HOLDER_HELPER_CODEC
-		.fieldOf("density_function")
-		.xmap(DensityFunctionCountPlacement::new, dfCountPlacement -> dfCountPlacement.densityFunction)
-		.codec();
-
-	protected int count(PlacementContext context, BlockPos pos) {
-		ChunkGenerator generator = context.generator();
-		NoiseGeneratorSettings generatorSettings = generator instanceof NoiseBasedChunkGenerator noiseBasedGenerator
-			? noiseBasedGenerator.generatorSettings().value()
-			: NoiseGeneratorSettings.dummy();
-
-		RandomState randomState = RandomState.create(
-			generatorSettings,
-			context.getLevel().registryAccess().registryOrThrow(Registry.NOISE_REGISTRY),
-			context.getLevel().getSeed()
-		);
-		DensityFunction.FunctionContext fnContext = new DensityFunction.SinglePointContext(pos.getX(), pos.getY(), pos.getZ());
-
-		double result = densityFunction.mapAll(
-			new KdlyExtrasDensityFunctionVisitor(randomState)
-		).compute(fnContext);
-		KdlyExtras.LOGGER.info("Function called! {}, {}", result, pos);
-		return Mth.floor(result);
-	}
+	public static final Codec<DensityFunctionCountPlacement> CODEC = RecordCodecBuilder.create(
+		instance -> densityFunctionField(instance).apply(instance, DensityFunctionCountPlacement::new)
+	);
 
 	@Override
-	public Stream<BlockPos> getPositions(PlacementContext context, RandomSource random, BlockPos pos) {
-		return IntStream.range(0, this.count(context, pos)).mapToObj(i -> pos);
+	public Stream<BlockPos> getPositions(PlacementContext context, RandomSource random, BlockPos pos, double result) {
+		return IntStream.range(0, Mth.floor(result)).mapToObj(i -> pos);
 	}
 
 	@Override
---
+++ src/main/java/artemis/kdlyextras/worldgen/DensityFunctionPlacementModifier.java
@@ -0,0 +1,56 @@
+package artemis.kdlyextras.worldgen;
+
+import com.mojang.datafixers.Products.P1;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder.Instance;
+import com.mojang.serialization.codecs.RecordCodecBuilder.Mu;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Registry;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.chunk.ChunkGenerator;
+import net.minecraft.world.level.levelgen.DensityFunction;
+import net.minecraft.world.level.levelgen.NoiseBasedChunkGenerator;
+import net.minecraft.world.level.levelgen.NoiseGeneratorSettings;
+import net.minecraft.world.level.levelgen.RandomState;
+import net.minecraft.world.level.levelgen.placement.PlacementContext;
+import net.minecraft.world.level.levelgen.placement.PlacementModifier;
+import net.minecraft.world.level.levelgen.placement.PlacementModifierType;
+
+import java.util.stream.Stream;
+
+public abstract class DensityFunctionPlacementModifier extends PlacementModifier {
+	public static final Codec<PlacementModifier> CODEC = Registry.PLACEMENT_MODIFIERS.byNameCodec().dispatch(PlacementModifier::type, PlacementModifierType::codec);
+	protected final DensityFunction densityFunction;
+
+	public DensityFunctionPlacementModifier(DensityFunction densityFunction) {
+		this.densityFunction = densityFunction;
+	}
+
+	protected static <P extends DensityFunctionPlacementModifier> P1<Mu<P>, DensityFunction> densityFunctionField(Instance<P> instance) {
+		return instance.group(
+			DensityFunction.HOLDER_HELPER_CODEC.fieldOf("density_function").forGetter(dfpm -> dfpm.densityFunction)
+		);
+	}
+
+	public abstract Stream<BlockPos> getPositions(PlacementContext context, RandomSource random, BlockPos pos, double densityFunctionResult);
+
+	@Override
+	public Stream<BlockPos> getPositions(PlacementContext context, RandomSource random, BlockPos pos) {
+
+		ChunkGenerator generator = context.generator();
+		NoiseGeneratorSettings generatorSettings = generator instanceof NoiseBasedChunkGenerator noiseBasedGenerator
+			? noiseBasedGenerator.generatorSettings().value()
+			: NoiseGeneratorSettings.dummy();
+
+		RandomState randomState = RandomState.create(
+			generatorSettings,
+			context.getLevel().registryAccess().registryOrThrow(Registry.NOISE_REGISTRY),
+			context.getLevel().getSeed()
+		);
+		DensityFunction.FunctionContext fnContext = new DensityFunction.SinglePointContext(pos.getX(), pos.getY(), pos.getZ());
+
+		double result = densityFunction.mapAll(new KdlyExtrasDensityFunctionVisitor(randomState)).compute(fnContext);
+
+		return getPositions(context, random, pos, result);
+	}
+}
