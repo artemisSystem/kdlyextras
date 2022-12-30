f184ef3232c790063482792c7f9115f7129b716d
Artemis System <theArtemisSystem@gmail.com>
2022-12-30 14:24:01 +0100

    Revert lib39-dessicant commits
    Couldn't get it to work, so shelving it for now

--- src/main/java/artemis/kdlyextras/KdlyContentInit.java
+++ src/main/java/artemis/kdlyextras/KdlyContentInit.java
@@ -1,10 +1,6 @@
 package artemis.kdlyextras;
 
-import com.unascribed.lib39.dessicant.api.DessicantControl;
-
 public class KdlyContentInit implements Runnable {
 	@Override
-	public void run() {
-		DessicantControl.optIn("kdlycontent");
-	}
+	public void run() {}
 }
