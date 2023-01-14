b56ee8773092d6358d0fbb9a91324a386bd854bb
Artemis System <theArtemisSystem@gmail.com>
2023-01-14 22:04:00 +0100

    Simplify RecombiningSlabsEntry codecs and make it a record
    Referencing quilt's ReversableBlockEntry. This is way cleaner

--- src/main/java/artemis/kdlyextras/RecombiningSlabsEntry.java
+++ src/main/java/artemis/kdlyextras/RecombiningSlabsEntry.java
@@ -9,48 +9,25 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 
-public class RecombiningSlabsEntry {
-	private static final Codec<BlockState> DEFAULT_BLOCKSTATE_CODEC = Registry.BLOCK.byNameCodec().xmap(
-		Block::defaultBlockState,
-		BlockState::getBlock
-	);
-	private static final Codec<BlockState> BLOCKSTATE_CODEC = Codec.either(BlockState.CODEC, DEFAULT_BLOCKSTATE_CODEC).xmap(
-		either -> either.map(l -> l, r -> r),
-		Either::left
+public record RecombiningSlabsEntry(BlockState result, boolean useSlabAxis) {
+	private static final Codec<BlockState> BLOCKSTATE_CODEC = Codec.either(BlockState.CODEC, Registry.BLOCK.byNameCodec()).xmap(
+		either -> either.map(state -> state, Block::defaultBlockState),
+		state -> state.equals(state.getBlock().defaultBlockState()) ? Either.right(state.getBlock()) : Either.left(state)
 	);
 
-	private static final Codec<RecombiningSlabsEntry> KEEP_AXIS_CODEC = RecordCodecBuilder.create(
-		instance -> instance.group(
-			BLOCKSTATE_CODEC.fieldOf("result").forGetter(rse -> rse.result),
-			Codec.BOOL.fieldOf("keep_axis").forGetter(rse -> rse.keepAxis)
-		).apply(instance, RecombiningSlabsEntry::new)
+	public static final Codec<RecombiningSlabsEntry> CODEC = Codec.either(
+			RecordCodecBuilder.<RecombiningSlabsEntry>create(instance -> instance.group(
+				BLOCKSTATE_CODEC.fieldOf("result").forGetter(rse -> rse.result),
+				Codec.BOOL.fieldOf("use_slab_axis").forGetter(rse -> rse.useSlabAxis)
+			).apply(instance, RecombiningSlabsEntry::new)),
+			BLOCKSTATE_CODEC).xmap(
+		either -> either.map(rse -> rse, state -> new RecombiningSlabsEntry(state, false)),
+		rse -> rse.useSlabAxis ? Either.left(rse) : Either.right(rse.result)
 	);
-
-	private static final Codec<RecombiningSlabsEntry> SIMPLE_CODEC = BLOCKSTATE_CODEC.xmap(
-		RecombiningSlabsEntry::new,
-		rse -> rse.result
-	);
-
-	public static final Codec<RecombiningSlabsEntry> CODEC = Codec.either(KEEP_AXIS_CODEC, SIMPLE_CODEC).xmap(
-		either -> either.map(l -> l, r -> r),
-		Either::left
-	);
-
-	private final BlockState result;
-	private final boolean keepAxis;
-
-	private RecombiningSlabsEntry(BlockState result, boolean keepAxis) {
-		this.result = result;
-		this.keepAxis = keepAxis;
-	}
-
-	private RecombiningSlabsEntry(BlockState result) {
-		this.result = result;
-		this.keepAxis = false;
-	}
 
 	public BlockState getNewState(Direction.Axis axis) {
 		BlockState state = this.result;
+		if (!useSlabAxis) return state;
 		if (state.hasProperty(BlockStateProperties.AXIS)) {
 			state = state.setValue(BlockStateProperties.AXIS, axis);
 		}
